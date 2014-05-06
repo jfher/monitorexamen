5f06314ebff9c43b6f610cabcbaf9dd980bf9d20
Luis Maldonado <luisoscar.mc@gmail.com>
2014-05-05 21:43:29 -0400

    vistaprincipalyadmi

--- config/routes.rb
+++ config/routes.rb
@@ -6,7 +6,8 @@ ThermMonitor::Application.routes.draw do
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
-
+  get '/home' => 'thermostats#home'
+  get '/admi' => 'thermostats#admi'
   # You can have the root of your site routed with "root"
    root 'thermostats#index'
 
