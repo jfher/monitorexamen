f6c1d10e880e640242986321a7f3ebd417cbcd70
irmp <hola>
2014-05-29 11:26:29 -0400

    configuracion alarma incompleto

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140528162114) do
+ActiveRecord::Schema.define(version: 20140529151554) do
+
+  create_table "alarms", force: true do |t|
+    t.integer  "max"
+    t.integer  "min"
+    t.integer  "time"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "history_thermostats", force: true do |t|
     t.integer  "temperature"
