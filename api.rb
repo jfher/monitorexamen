06519d8bc8a9d9ea885986f13f418bbf4c5c6caa
Luis Maldonado <luisoscar.mc@gmail.com>
2014-06-02 19:01:51 -0400

    ultimo

--- api.rb
+++ api.rb
@@ -5,9 +5,9 @@ require 'json'
 
 agent = Mechanize.new
 	a = agent.post('http://localhost:3000/history_thermostats.json',{
-			"history_thermostat[temperature]" => 35, 
+			"history_thermostat[temperature]" => 70, 
 			"history_thermostat[humidity]" => 100,
 			"history_thermostat[energy]" => 8,
-			"history_thermostat[thermostat_id]" => 1,
-			 mail: 'luis@yahoo.com' , pass: 'usuariotres'
+			"history_thermostat[thermostat_id]" => 6,
+			 mail: 'oscar@yahoo.com' , pass: 'usuariodos'
 			})
