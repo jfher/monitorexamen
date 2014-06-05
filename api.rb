7d8677dc01e256274ebb95c54633e839e5bb1dc7
DenysDenylson <DenysDenylson@gmail.com>
2014-06-05 12:43:33 -0400

    alertas terminado

---
+++ api.rb
@@ -0,0 +1,13 @@
+require 'rubygems'
+require 'mechanize'
+require 'nokogiri'
+require 'json'
+
+agent = Mechanize.new
+	a = agent.post('http://localhost:3000/history_thermostats.json',{
+			"history_thermostat[temperature]" => 10, 
+			"history_thermostat[humidity]" => 10,
+			"history_thermostat[energy]" => 10,
+			 serial: '111', #numero serial del termostato
+			 mail: 'user3@gmail.com' , pass: '13245678' #usuario y contraseña del dueño del termostato
+			})
