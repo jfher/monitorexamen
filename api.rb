cb2864fb29ee1820cef5683ce140492d8280fede
DenysDenylson <DenysDenylson@gmail.com>
2014-06-05 13:01:52 -0400

    error corregido

--- api.rb
+++ api.rb
@@ -5,9 +5,9 @@ require 'json'
 
 agent = Mechanize.new
 	a = agent.post('http://localhost:3000/history_thermostats.json',{
-			"history_thermostat[temperature]" => 10, 
+			"history_thermostat[temperature]" => 120, 
 			"history_thermostat[humidity]" => 10,
 			"history_thermostat[energy]" => 10,
-			 serial: '111', #numero serial del termostato
-			 mail: 'user3@gmail.com' , pass: '13245678' #usuario y contraseña del dueño del termostato
+			 serial: '11', #numero serial del termostato
+			 mail: 'user2@gmail.com' , pass: '13245678' #usuario y contraseña del dueño del termostato
 			})
