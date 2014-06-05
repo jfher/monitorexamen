8c41e476d787acefa20652deaa8dcc29b9cfbc12
Luis Maldonado <luisoscar.mc@gmail.com>
2014-06-04 22:23:55 -0400

    seeds_completo

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,14 +5,18 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
-main_admin = User.create(:name => 'System', :last_name => 'Admin', 
-       :email => 'admin@gmail.com', :password => '12345678', 
-       :password_confirmation => '12345678', :role => 'admin').save(:validate => false)
-user1 = User.create(:id => '33',:name => 'System', :last_name => 'Admin', 
-       :email => 'user1@gmail.com', :password => '12345678', 
-       :password_confirmation => '12345678', :role => 'simple').save(:validate => false)
-user2 = User.create(:name => 'System', :last_name => 'Admin', 
-       :email => 'user2@gmail.com', :password => '12345678', 
-       :password_confirmation => '12345678', :role => 'simple').save(:validate => false)
-termostat1 = Thermostat.create(:serial => '1111',:id => '1',:user_id => '33',:temperature => '30',:humidity => '20',:location_id => '17')
-location1= Location.create(:id => '17',:name => 'oficina',:address => 'ayacucho',:user_id => '33')
+User.new({:email => "admin@thermmonitor.com.bo", :password => "usuarioadmin", :password_confirmation => "usuarioadmin" }).save(:validate => false)
+User.new({:email => "user1@thermmonitor.com.bo", :password => "usuariouno", :password_confirmation => "usuariouno" }).save(:validate => false)
+User.new({:email => "user2@thermmonitor.com.bo", :password => "usuariodos", :password_confirmation => "usuariodos" }).save(:validate => false)
+Location.new({:name => "Casa", :address => "Cochabamba, Av. Circunvalacion", :user_id => 2}).save(:validate => true)
+Location.new({:name => "Oficina", :address => "Cochabamba, Av. Pando", :user_id => 2}).save(:validate => true)
+Location.new({:name => "Ferreteria", :address => "Cochabamba, Av. Libertadores", :user_id => 3}).save(:validate => true)
+Location.new({:name => "Carniceria", :address => "Cochabamba, Av. America", :user_id => 3}).save(:validate => true)
+Thermostat.new({:serial => "123", :temperature => "30", :user_id => 2, :energy => 10, :location_id => 1}).save(:validate => false)
+Thermostat.new({:serial => "1234", :temperature => "50", :user_id => 2, :energy => 20, :location_id => 1}).save(:validate => false)
+Thermostat.new({:serial => "12345", :temperature => "40", :user_id => 2, :energy => 30, :location_id => 2}).save(:validate => false)
+Thermostat.new({:serial => "123556", :temperature => "10", :user_id => 2, :energy => 40, :location_id => 2}).save(:validate => false)
+Thermostat.new({:serial => "0012", :temperature => "30", :user_id => 3, :energy => 10, :location_id => 3}).save(:validate => false)
+Thermostat.new({:serial => "00123", :temperature => "50", :user_id => 3, :energy => 20, :location_id => 3}).save(:validate => false)
+Thermostat.new({:serial => "001234", :temperature => "40", :user_id => 3, :energy => 30, :location_id => 4}).save(:validate => false)
+Thermostat.new({:serial => "0012345", :temperature => "10", :user_id => 3, :energy => 40, :location_id => 4}).save(:validate => false)
