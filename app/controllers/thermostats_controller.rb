67e878d88ac7a969b3f44377528920e2e761bb39
irmp <hola>
2014-06-14 22:53:06 -0400

    merge default schema

--- app/controllers/thermostats_controller.rb
+++ app/controllers/thermostats_controller.rb
@@ -239,6 +239,6 @@ end
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def thermostat_params
-      params.require(:thermostat).permit(:serial, :temperature, :humidity, :energy, :user_id, :location_id)
+      params.require(:thermostat).permit(:serial, :temperature, :humidity, :energy, :user_id, :location_id, :default_temperature)
     end
 end
