4cb52b3892e81e1340aaef1bcfb31f82ed98a8ca
Luis Maldonado <luisoscar.mc@gmail.com>
2014-06-17 03:16:11 -0400

    ultimo

--- api.rb
+++ api.rb
@@ -5,10 +5,10 @@ require 'json'
 
 agent = Mechanize.new
 	a = agent.post('http://localhost:3000/history_thermostats.json',{
-			"history_thermostat[temperature]" => 35,  
+			"history_thermostat[temperature]" => 40,  
 			"history_thermostat[humidity]" => 10,
 			"history_thermostat[energy]" => 10,
-			 serial: '777', #numero serial del termostato
+			 serial: '123', #numero serial del termostato
 			 mail: 'user1@thermmonitor.com.bo' , pass: 'usuariouno' #usuario y contraseña del dueño del termostato
 			})
 
